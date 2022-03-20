2da5b6cbb42838982ed6d87a0e07f2fc7bdc0eec
lnd4812 <laureldavid64@gmail.com>
2022-03-19 23:17:33 -0400

    commit coding changes after hopefully resolving repository issues

--- mod-4-quiz-challenge/assets/js/script.js
+++ mod-4-quiz-challenge/assets/js/script.js
@@ -4,13 +4,12 @@
 var startBtn = document.querySelector("#start");
 
 // create "containers" for each page
-var doneEl = document.createElement("div");
-var quizQuestionSetEl = document.createElement("div");
-var highScoreDisplayEl = document.createElement("div");
-var  = document.createElement("div");
+var quizQuestionSetEl = document.querySelector("#quiz-questions");
+var highScoreDisplayEl = document.querySelector("#high-scores");
 var formEl = document.createElement("#highScoreDisplayForm");
-var timerEl = document.getElementById("countdown");
+var timerEl = document.getElementById("");
 var quizTime = 60;
+var score = 0;
 
 var quizQuestionsEl= ;
 
@@ -19,33 +18,90 @@ var quizQuestionsEl= ;
 
 
 // timer has been reset and countdown begins when user starts quiz
-function countdown = () {
-var quizTime = 60;
-
-var startQuiz = setInterval(function() {
+function quizTimer = () {
+// section is not visible until start button clicked
+  quizQuestionsSetEl.style.display = "none";
+// call first question, position 0 in array, to start quiz
+questionNumber(0);
+var quizTimeDisplay = setInterval(function() {
 // user answers questions until finished or time runs out
-while (quizTime > 0)
-// function to proceed through list of questions 
-var 
+  If (quizTime <= 0 || quizQuestions.length === 5) 
+  // user is out of time or finished the quiz, 
+  { clearInterval(quizTimeDisplay); 
+    document.getElementById("timer").textContent = "The quiz has ended";
+    // go to scorer Initial Input field
+    }  else {
+      document.getElementById("timer").textContent = "Time remaining";
+    } 
+ // decrease time by 1 second
+quizTime = quizTime--
+  }, 1000);
+};
+ 
+// quiz question process 
+function quizRunThrough() {// function and # of question)
+  if (quizTime > 0 && quizQuestions.length < 5) {
+    document.querySelector()//text.content the question at the index #
+    // set answer selection to empty
+    answerOptionEl.innerHTML = ""; // can it just be ""?
+  }
+ 
 for(var i =0; i < quizQuestions.length; i++ ) {
+  var option = document.createElement('button');
+  option.setAttribute("class", "option-btn"); 
+  // review what this is
+  option.setAttribute("value", questions[number].choices[]);
+  // add coding to change button color on click, check answer and response correct or wrong
+} else {
+  // call function to take user to All Done page
+  inputScore();
+}
 
-}      
-quizQuestionSetEl 
 
+// create array to store questions and answers with inner array for quiz choices     
+quizQuestionSetEl = 
+{
+  question: "Commonly used data types do NOT include:",
+  choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+  correctChoice: "Alerts"
+},
+{
+  question: "The condition in an if/else statement is enclosed in:",
+  choices: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
+  correctChoice: "Parenthesis"
+},
+{
+  question: "Arrays in JavaScript can be used to store:",
+  choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+  correctChoice: "All of the above"
+},
+{
+  question: "String values must be enclosed within ______ when being assigned to variables",
+  choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+  correctChoice: "Quotes"
+},
+{
+  question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  choices: ["JavaScript", "Terminal Bash", "for loops", "console.log"],
+  correctChoice: "console.log"
+
+}
 // countdown for user to take quiz  (place in div in upper right hand corner)
   timerEl.textContent = quizTime + ' seconds';
   //decrement timer each second
   quizTime--;
 }
-// if user answers question incorrectly, display "wrong" beneath <ol> for question and subtract 10 secons from time
-
-// if user answers question correctly, proceed to next set of questions
+// if user answers question incorrectly, display "wrong" beneath 
+// create element with text.content - wrong answer. you lose 10 seconds, quizTime - 10
+// if user answers question correctly, display correct and proceed to next question - use index #
 
 // if time = 0 before user has finished answering all questions, highScore value = 0.  display "try again next time"
 
 // if time > 0 and user has answered all questions, display final score based on quiz-time 
+// add function to end game and go to next 
 var highScoreTime = document.querySelector("#quiz-time");
-
+// style display = "none";
+// then style display = "inherit"
   }
 countdown();
 // User inputs initials after finishing quiz and initials are stored with highScoreTime in local storage
@@ -53,8 +109,7 @@ var highScorerInit = document.querySelector("#high-scorer");
 var addScoreButton = document.querySelector("#add-score");
 
 addScore.addEventListener("click", function(event) {
-  event.preventDefault();
-  
+    
   // create highScorer record from submission
   var highScorer = {
     highScoreTime: highScoreTime.value.trim(),
@@ -101,17 +156,15 @@ highScoreDisplayEl.appendChild(clearHighScoresButtonEl);
 // Create ordered list element for each set of 4 questions
 var listEl = document.createElement("ol");
 // Create ordered list items
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+
 
 // appendChild differently for each question?
 };
+
 startQuiz();
 
 
-// Create a new task
+// add event listener to start quiz when button clicked
 startBtn.addEventListener("click", startQuiz);
   
 // for edit and delete buttons
