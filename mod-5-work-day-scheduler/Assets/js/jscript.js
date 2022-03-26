b712ab15440117b57f4faee3a8558d785b7abe2a
lnd4812 <laureldavid64@gmail.com>
2022-03-26 09:23:38 -0400

    add additional link.script cdns

---
+++ mod-5-work-day-scheduler/Assets/js/jscript.js
@@ -0,0 +1,9 @@
+// use momentum.js to display current date & time under "a simple calendar app..."
+
+// create timeblocks to add to "container"; each should have "hour" on left
+
+// add modal to enter information by date, set that item up for saving in local storage
+
+// add css that changes time block colour based on date/time of task compared to current time
+
+// add "save" button
